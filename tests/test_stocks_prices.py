094bf8bfd6556dba7107fb6412de73eabf67dcb2
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-07-06 21:19:28 +0500

    created tests for get_stocks_prices

---
+++ tests/test_stocks_prices.py
@@ -0,0 +1,42 @@
+import os
+from unittest.mock import patch, MagicMock
+
+from src.utils import get_stocks_prices
+
+
+def test_stocks_prices_empty_list():
+    """Тестирование случая когда список акций пуст"""
+    assert  get_stocks_prices([]) == []
+
+
+def test_stocks_prices_error_request():
+    """Тестирование случая когда запрос к сервису не удался"""
+    mock_response = MagicMock()
+    mock_response.status_code = 404
+    mock_response.json.return_value = {'ticker': 'AMZN', "price": 100}
+    url = os.getenv('URL')
+    apy_key = os.getenv('API_KEY')
+    params = {'ticker': 'AMZN'}
+    headers = {'X-Api-Key': apy_key}
+    with patch('requests.get') as mock_get:
+        mock_get.return_value = mock_response
+        assert get_stocks_prices(['AMZN']) == []
+        mock_get.assert_called_once_with(url, headers=headers, params=params)
+
+
+def test_stocks_prices():
+    """Тестирование успешного получения курсов валют"""
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {'ticker': 'AMZN', "price": 100}
+    url = os.getenv('URL')
+    apy_key = os.getenv('API_KEY')
+    params = {'ticker': 'AMZN'}
+    headers = {'X-Api-Key': apy_key}
+    with patch('requests.get') as mock_get:
+        mock_get.return_value = mock_response
+        assert get_stocks_prices(['AMZN']) == [{
+                                                 "stock":'AMZN',
+                                                 "price": 100
+                                               }]
+        mock_get.assert_called_once_with(url, headers=headers, params=params)
